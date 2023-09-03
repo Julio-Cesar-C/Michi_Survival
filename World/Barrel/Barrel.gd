e86b8c23a6f6772ad171351616f4c4fa027b3175
Mateus <mateus.dias029@gmail.com>
2023-09-03 14:12:00 -0300

    add explosion sound effect

--- World/Barrel/Barrel.gd
+++ World/Barrel/Barrel.gd
@@ -1,18 +1,19 @@
 extends StaticBody2D
 
+@onready var explosion_sound = $explosion_sound
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
 func _on_area_2d_body_entered(object):
 	if "@CharacterBody2D@" in object.name:
 		explosao()
+		explosion_sound.play()#reproduz som explosao
 	pass # Replace with function body.
 	
 
@@ -23,5 +24,5 @@ func explosao():
 			object.queue_free()
 	queue_free()
 	
-	var audio_player = $explosion_sound
-	audio_player.play()
+
+
